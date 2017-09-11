baf3fd68aacc707b82129dca45df9e5f464d7711
Arturo Iglesias <iglesias294@gmail.com>
2017-09-11 12:08:20 +0000

    added server sie code for pro users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to:'pages#home'
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   get 'about', to: 'pages#about'
   get 'testpage', to: 'pages#testpage'
   resources :contacts, only: :create
