e8382cfe239eebb4252185c0b0ec6be9dcb2ae66
Arturo Iglesias <iglesias294@gmail.com>
2017-09-14 01:44:00 +0000

    added profiles controller, new view, and link on homepage

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,8 @@
+class ProfilesController < ApplicationController
+    
+    #user makes a get request to /users/:user_id/profile/new
+    def new
+    # Render blank profile details form 
+    end
+    
+end
