1dc37db2de598d484cb7ef3c5a0fc34624f6111d
Arturo Iglesias <iglesias294@gmail.com>
2017-09-25 09:36:13 +0000

    added user profile form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,7 @@ class ProfilesController < ApplicationController
     #user makes a get request to /users/:user_id/profile/new
     def new
     # Render blank profile details form 
+    @profile = Profile.new
     end
     
 end
