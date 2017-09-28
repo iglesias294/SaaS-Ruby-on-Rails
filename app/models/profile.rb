9a6efc6d3bd65e5187743f82138538311d04b0a8
Arturo Iglesias <iglesias294@gmail.com>
2017-09-28 09:20:25 +0000

    added file field for image upload to profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,3 +5,4 @@ class Profile < ActiveRecord::Base
                       :defualt_url => "/images/:style/missing.png"
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
+ 
