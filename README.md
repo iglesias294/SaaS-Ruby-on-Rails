e1321b6e99a8acd39643a2d6d6d9edd391008892
Arturo Iglesias <iglesias294@gmail.com>
2017-11-26 14:07:28 +0000

    tweak db

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # README
-[Click here to see this app](https://young-dawn-16325.herokuapp.com/) 
+[Click here to see this app live on Heroku](https://young-dawn-16325.herokuapp.com/) 
 
 This is an example SaaS app built as part of a Ruby on Rails tutorial. 
 It features user login and profiles with Devise, payment processing with
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  belongs_to :plan
+  belongs_to :plan, optional: true
   has_one :profile
   
   attr_accessor :stripe_card_token
